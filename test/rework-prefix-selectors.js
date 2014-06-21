584ce452c963498d854ffe719e9351ee38233f46
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-06-21 17:42:01 -0400

    Update deps and tests

--- test/rework-prefix-selectors.js
+++ test/rework-prefix-selectors.js
@@ -1,5 +1,5 @@
 var rework = require('rework')
-  ,  mixin = require('../')
+  , prefixSelectors = require('../')
   , fs = require('fs')
   , assert = require('assert')
   , read = fs.readFileSync;
@@ -11,13 +11,13 @@ function fixture (name) {
 describe('.prefixSelectors(str)', function(){
   it('should prefix selectors', function(){
     rework(fixture('prefix-selectors'))
-      .use(rework.prefixSelectors('#dialog'))
+      .use(prefixSelectors('#dialog'))
       .toString()
       .should.equal(fixture('prefix-selectors.out'));
   })
   it('should use the prefix as the :root pseudo-class', function(){
     rework(fixture('prefix-selectors-root'))
-      .use(rework.prefixSelectors('#dialog'))
+      .use(prefixSelectors('#dialog'))
       .toString()
       .should.equal(fixture('prefix-selectors-root.out'));
   })
